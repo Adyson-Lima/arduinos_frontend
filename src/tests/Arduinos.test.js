562a8d918d071e0df9b44957b2ef17d4602e4cfc
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-13 16:15:54 -0300

    6° commit - tdd tela Arduinos: Existe tabela em Arduinos? RED

--- src/tests/Arduinos.test.js
+++ src/tests/Arduinos.test.js
@@ -20,4 +20,8 @@ describe('testes da tela Arduinos', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Arduinos?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
